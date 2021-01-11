cf6ef40effe0572a9aa1323452256458e5ab1cc7
Jon Fossheim <fossheimjoneven@gmail.com>
2021-01-11 10:04:28 +0100

    refactor: languages and api url added

---
+++ src/utils/utils.js
@@ -0,0 +1,10 @@
+export const apiUrl = 'https://api.dictionaryapi.dev/api/v2/entries/';
+export const languages = [
+   { code: 'en', language: 'english' },
+   { code: 'es', language: 'spanish' },
+   { code: 'fr', language: 'french' },
+   { code: 'de', language: 'german' },
+   { code: 'it', language: 'italian' },
+   { code: 'pt-BR', language: 'brazilian portuguese' },
+   { code: 'tr', language: 'turkish' },
+];
